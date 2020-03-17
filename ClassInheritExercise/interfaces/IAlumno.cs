4682198705587abd597ee9bdb39b04c033776424
juankiduran12@gmail.com <juankiduran12@gmail.com>
2020-03-17 12:06:58 +0100

    Ejer1

---
+++ ClassInheritExercise/interfaces/IAlumno.cs
@@ -0,0 +1,21 @@
+﻿using ClassInheritExercise.clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritExercise.interfaces
+{
+    public interface IAlumno
+    {
+        List<Asignatura> asignaturasMAtriculadas { get; set; }
+        string rama { get; set; }
+        int curso { get; set; }
+        char letra { get; set; }
+
+        void SeMAtriculaAsignatura();
+        void EstudiaAsignatura();
+        bool SeDivierte();
+    }
+}
---
+++ ClassInheritExercise/interfaces/IProfesor.cs
@@ -0,0 +1,19 @@
+﻿using ClassInheritExercise.clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritExercise.interfaces
+{
+    public interface IProfesor
+    {
+        string departamento { get; set; }
+        int id { get; set; }
+        List<Asignatura> asignaturaQueImparte { get; set; }
+
+        void ImparteAsignatura();
+        void ExaminaAlumnos();
+    }
+}
