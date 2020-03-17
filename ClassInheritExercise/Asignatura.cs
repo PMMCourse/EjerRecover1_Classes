f695dd160818cc810298d39c60fa2d6e409d1280
jlrubial <jlrubial94@gmail.com>
2020-03-17 17:23:38 +0100

    Ejercicio1

---
+++ ClassInheritExercise/Asignatura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassInheritExercise
+{
+    public class Asignatura : IAsignatura
+    {
+        public string nombre { get; set; }
+        public int codigo { get; set; }
+
+        public Asignatura(string nombre, int codigo)
+        {
+            this.nombre = nombre;
+            this.codigo = codigo;
+        }
+
+        public Asignatura()
+        {
+
+        }
+    }
+}
---
+++ ClassInheritExercise/IPersona.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassInheritExercise
+{
+    public interface IPersona
+    {
+        string apellido { get; set; }
+        string dni { get; set; }
+        string nombre { get; set; }
+    }
+}
